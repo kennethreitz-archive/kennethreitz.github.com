4844b3b2f8df8c0a4530bcb2f8dad12bd7ccf96b
Kenneth Reitz <me@kennethreitz.com>
2010-04-21 04:30:48 -0400

    update

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,18 @@
 from fabric.api import *
 
 def build():
-    """ Do it. 
-    """
-    local("ronn -5 man/kennethreitz.1.ron > index.html")
+	""" Do it. 
+	"""
+	local("ronn -5 man/kennethreitz.1.ron > index.html")
 
 def deploy():
 	""" Activate local virtualenv
 	"""
 	local("git commit -am 'update'")
 	local("git push")
+
+def go():
+	""" Do it. 
+	"""
+	build()
+	deploy()
